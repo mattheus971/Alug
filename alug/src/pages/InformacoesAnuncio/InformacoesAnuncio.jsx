10c7431eaf915dab506636d6868bb99dee045db6
MATHEUS DE VASCONCELOS VELOZO <matheus_velozo@estudante.sesisenai.org.br>
2025-12-03 22:06:17 -0300

    Progresso

--- alug/src/pages/InformacoesAnuncio/InformacoesAnuncio.jsx
+++ alug/src/pages/InformacoesAnuncio/InformacoesAnuncio.jsx
@@ -4,24 +4,64 @@ import Cabecalho from '../../components/Cabecalho/Cabecalho'
 function InformacoesAnuncio() {
   return (
     <>
-    <Cabecalho />
-    <div className='wrapper-anuncio'>
-      
+      <Cabecalho />
+      <div className='wrapper-anuncio'>
 
-      <div className='cntnr-esquerda-infos-anuncio'>
-        <div className='inputs-fotos'>
+
+        <div className='cntnr-esquerda-infos-anuncio'>
+          <div className='containerpai-inputs-fotos'>
+
+            <div className='linha-um'>
+              <div className="container-input-foto"></div>
+              <div className="container-input-foto"></div>
+            </div>
+
+            <div className='linha-dois'>
+              <div className="container-input-foto"></div>
+              <div className="container-input-foto"></div>
+            </div>
+
+          </div>
         </div>
-        <div className='inputs-fotos'>
+
+        <div className='cntnr-direita-infos-anuncio'>
+          <form className='form-inputs-criar-anuncio'>
+            
+            <div className='ctnr-zxlj'>
+              <label htmlFor="titulo-anuncio">Titulo do Anuncio</label>
+              <input type="text" />
+            </div>
+
+            <div className='ctnr-zxlj' >
+              <label htmlFor="titulo-anuncio">Descrição</label>
+              <textarea type="text" />
+            </div>
+
+            <div className='ctnr-zxlj' >
+              <label htmlFor="titulo-anuncio">Titulo do anuncio</label>
+              <input type="text" />
+            </div>
+
+            <div className='ctnr-zxlj' >
+              <label htmlFor="titulo-anuncio">Tipo do imóvel</label>
+              <input type="text" />
+            </div>
+
+            <div className='ctnr-zxlj' >
+              <label htmlFor="titulo-anuncio">Tipo do imóvel</label>
+              <input type="text" />
+            </div>
+
+            <div className='ctnr-zxlj' >
+              <label htmlFor="titulo-anuncio">Tipo do imóvel</label>
+              <input type="text" />
+            </div>
+
+          </form>
         </div>
-
       </div>
-
-      <div className='cntnr-direita-infos-anuncio'>
-        <h1>A</h1>
-      </div>
-    </div>
     </>
-    
+
   )
 }
 
